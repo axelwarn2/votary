b560d0ab3d0436408639627da164d899ef744817
axelwarn2 <722shock722@mail.ru>
2025-03-08 01:54:25 +0500

    13473: Добавит детальную страницу статистики

---
+++ frontend/src/components/steps/RecordStatisticDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="record-notes-detail">
+    <h3 class="record-notes-detail__title">Эффективность: Пятков Константин</h3>
+
+      <div class="efficiency-stats">
+        <div class="efficiency-stats__overview">
+          <h3 class="efficiency-stats__overview-title">Эффективность</h3>
+          <div class="efficiency-stats__image"></div>
+        </div>
+        <div class="efficiency-stats__in-progress">
+          <h3 class="efficiency-stats__in-progress-title">Всего в работе</h3>
+          <p class="efficiency-stats__in-progress-value">3</p>
+        </div>
+        <div class="efficiency-stats__completed">
+          <h3 class="efficiency-stats__completed-title">Завершено</h3>
+          <p class="efficiency-stats__completed-value">2</p>
+        </div>
+        <div class="efficiency-stats__overdue">
+          <h3 class="efficiency-stats__overdue-title">Просрочено</h3>
+          <p class="efficiency-stats__overdue-value">1</p>
+        </div>
+        <div class="efficiency-stats__chart">
+          <div class="grap__image"></div>
+        </div>
+      </div>
+
+  </div>
+
+
+</template>
