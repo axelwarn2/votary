ebb971e39bd10a9dea7e20ceaa942805146fb327
axelwarn2 <722shock722@mail.ru>
2025-04-30 23:26:46 +0500

    13227: Добавит получение и отображение детальной информации сотрудника

--- src/app/backend/routes/employee.py
+++ src/app/backend/routes/employee.py
@@ -63,3 +63,31 @@ def get_employee_stats(db: Session = Depends(get_db)):
         for row in result
     ] 
     return employees_stats
+
+@router.get("/employee/{employee_id}")
+def get_employee(employee_id: int, db: Session = Depends(get_db)):
+    current_date = datetime(2025, 4, 30)
+    stmt = (
+        select(
+            Employee.id,
+            Employee.surname,
+            Employee.name,
+            func.count(Task.id).label("count_task"),
+            func.sum(case((Task.status == StatusEnum.выполнена, 1), else_=0)).label("complete"),
+            func.sum(case((Task.deadline < current_date, 1), else_=0)).label("expired"),
+        )
+        .outerjoin(Task, Employee.id == Task.employee_id)
+        .where(Employee.id == employee_id)
+        .group_by(Employee.id, Employee.surname, Employee.name)
+    )
+    result = db.execute(stmt).first()
+
+    return {
+        "id": result.id,
+        "surname": result.surname,
+        "name": result.name,
+        "count_task": result.count_task,
+        "complete": result.complete,
+        "expired": result.expired,
+        "efficiency": f"{random.randint(10, 100)}%"
+    }
--- src/app/frontend/src/components/steps/records/RecordStatisticDetail.vue
+++ src/app/frontend/src/components/steps/records/RecordStatisticDetail.vue
@@ -1,6 +1,29 @@
+<script setup>
+import { onMounted, ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import axios from 'axios';
+
+const route = useRoute();
+const statistic = ref(null);
+
+onMounted(async () => {
+    try {
+        const response = await axios.get(`http://localhost:8000/employee/${route.params.id}`);
+        statistic.value = response.data;
+    } catch (error) {
+        console.error("Ошибка получения детальной информации сотрудника: ", error);
+    }
+});
+
+const name = computed(() => {
+    return `${statistic.value?.surname} ${statistic.value?.name}`;
+})
+console.log(name);
+</script>
+
 <template>
-    <div class="record-notes-detail">
-        <h3 class="record-notes-detail__title">Эффективность: Пятков Константин</h3>
+    <div v-if="statistic" class="record-notes-detail">
+        <h3 class="record-notes-detail__title">Эффективность: {{ name }}</h3>
 
         <div class="efficiency-stats">
             <div class="efficiency-stats__overview">
@@ -9,7 +32,7 @@
                     <div class="efficiency-stats__circle--first">
                         <div class="efficiency-stats__circle--second">
                             <div class="efficiency-stats__circle--third">
-                                66%
+                                {{ statistic.efficiency }}
                             </div>
                         </div>
                     </div>
@@ -17,15 +40,15 @@
             </div>
             <div class="efficiency-stats__in-progress">
                 <h3 class="efficiency-stats__in-progress-title">Всего в работе</h3>
-                <p class="efficiency-stats__in-progress-value">3</p>
+                <p class="efficiency-stats__in-progress-value">{{ statistic.count_task }}</p>
             </div>
             <div class="efficiency-stats__completed">
                 <h3 class="efficiency-stats__completed-title">Завершено</h3>
-                <p class="efficiency-stats__completed-value">2</p>
+                <p class="efficiency-stats__completed-value">{{ statistic.complete }}</p>
             </div>
             <div class="efficiency-stats__overdue">
                 <h3 class="efficiency-stats__overdue-title">Просрочено</h3>
-                <p class="efficiency-stats__overdue-value">1</p>
+                <p class="efficiency-stats__overdue-value">{{ statistic.expired }}</p>
             </div>
         </div>
     </div>
